d7c4e3be5f49363040f3c7a97a29eaf0cfe57860
reddypandu <saipandupatt143@gmail.com>
2024-02-12 23:13:45 +0530

    modified:   about.html

--- js/main.js
+++ js/main.js
@@ -113,3 +113,36 @@
 })(jQuery);
 
 
+// const scriptURL = 'https://script.google.com/macros/s/AKfycbwxrr9gY3hHl86SuG0uv8nQa0NJHnmqFYqzCzrqM0w4wWDuE-B--j9r9iwch5FHZK6o/exec'
+
+// const form = document.forms['contact-form']
+
+// form.addEventListener('submit', e => {
+//   e.preventDefault()
+//   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+//   .then(response => alert("Thank you! your form is submitted successfully." ))
+//   .then(() => { window.location.reload(); })
+//   .catch(error => console.error('Error!', error.message))
+// })
+
+
+function runCode() {
+  var htmlCode = document.getElementById("html").value;
+  var cssCode = document.getElementById("css").value;
+  var jsCode = document.getElementById("javascript").value;
+  
+  var resultFrame = document.getElementById("result");
+  var result = resultFrame.contentDocument || resultFrame.contentWindow.document;
+  
+  result.open();
+  result.write(`
+    <html>
+    <head>
+      <style>${cssCode}</style>
+    </head>
+    <body>${htmlCode}</body>
+    <script>${jsCode}</script>
+    </html>
+  `);
+  result.close();
+}
